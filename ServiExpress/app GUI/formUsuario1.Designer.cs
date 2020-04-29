2fba619a089dc0b4f1a91e59096194743093af13
Fernando <fernandosepulvda@gmail.com>
2020-04-29 13:41:28 -0400

    Corregido archivo txt indicaciones

--- ServiExpress/app GUI/formUsuario1.Designer.cs
+++ ServiExpress/app GUI/formUsuario1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 44);
+            this.button1.Location = new System.Drawing.Point(12, 69);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 41);
             this.button1.TabIndex = 2;
@@ -43,19 +45,37 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 412);
+            this.button2.Location = new System.Drawing.Point(12, 116);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(133, 41);
             this.button2.TabIndex = 3;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 488);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(151, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(721, 460);
+            this.panel1.TabIndex = 5;
+            // 
             // formUsuario1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(884, 465);
+            this.ClientSize = new System.Drawing.Size(884, 541);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -71,5 +91,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
     }
 }
