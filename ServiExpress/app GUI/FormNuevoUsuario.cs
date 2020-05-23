d767f4debbec3e81ddeeaf98934db4e309261f25
Fernando <fernandosepulvda@gmail.com>
2020-05-22 22:00:43 -0400

    Se eliminan librerias sin usar en form nuevo usuario

--- ServiExpress/app GUI/FormNuevoUsuario.cs
+++ ServiExpress/app GUI/FormNuevoUsuario.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ServiExpress.app_GUI
@@ -43,6 +36,11 @@ namespace ServiExpress.app_GUI
         }
 
         private void txtTelefono_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardarUsuario_Click(object sender, EventArgs e)
         {
 
         }
